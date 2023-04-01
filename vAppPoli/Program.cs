f2d9168997885eaa5da25ac62f4e786d3451968f
Fernando Angulo <57889477+luisfernandoAngulo28@users.noreply.github.com>
2023-04-01 18:59:50 -0400

    Grupo SI
    Grupo SI

---
+++ vAppPoli/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace vApp
+{
+    class Program
+    {  
+        static void Main(string[] args)
+        {
+            clsNoti[] mensaje = new clsNoti[3];
+            mensaje[0] = new clsCorreo("fernando.fa671@gmail.com");
+            mensaje[1] = new clsSms("70252765");
+            mensaje[2] = new clsWsp("70252765");
+            for (int i = 0; i < mensaje.Length; i++)
+            {
+                mensaje[i].Mensaje = " hola soy yo...";
+                Console.WriteLine(mensaje[i].Enviar());
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ vAppPoli/clsNoti.cs
@@ -0,0 +1,106 @@
+﻿
+using System;
+
+namespace vApp
+{
+    //
+    public abstract class clsNoti
+    {
+        // ATRIBUTOS
+
+        public string _mesaje;
+
+        // DESCRIPTORES DE ACCESO
+        public string Mensaje
+        {
+            get { return _mesaje; }
+            set { _mesaje = value; }
+        }
+
+        //METODOS
+        public abstract string Enviar();
+    }
+
+ 
+    // Clase Correo
+    public class clsCorreo : clsNoti
+    {
+        string _correo;
+
+        public string Correo
+        {
+            get { return _correo; }
+            set { _correo = value; }
+        }
+
+        public clsCorreo()
+        {
+            this.Correo = "";
+        }
+        public  clsCorreo(string xcorreo)
+        {
+            this.Correo = xcorreo;
+        }
+
+        public override string Enviar()
+        {
+            string xnoti = "Mensaje : " + this.Mensaje + " enviado por mail a "+Correo;
+            return xnoti;
+        }
+    }
+
+    public class clsSms : clsNoti
+    {
+        string _telefono;
+
+        public string Telefono
+        {
+            get { return _telefono; }
+            set { _telefono = value; }
+        }
+
+        public clsSms()
+        {
+            this.Telefono = "";
+        }
+        public clsSms(string xtelefono)
+        {
+            this.Telefono = xtelefono;
+        }
+
+        public override string Enviar()
+        {
+            string xnoti = "Mensaje : " + this.Mensaje + " enviado por SMS a "+Telefono;
+            return xnoti;
+        }
+    }
+    public class clsWsp : clsNoti  //nombre de la clase:Padreclase
+    {
+        //Constructor
+        string _whatsapp;
+        //copía
+        public string Whatsapp
+        {
+            get { return _whatsapp; }
+            set { _whatsapp = value; }
+        }
+        //Constructor
+        public clsWsp()
+        {
+            this.Whatsapp = "";
+        }
+        //Constructor Con Parametros 
+        public clsWsp(string xwhatsapp)///70252765
+        {
+            this.Whatsapp = xwhatsapp;  //Whatsapp=70252765
+        }
+
+        public override string Enviar()
+        {
+            string xnoti = "Mensaje : " + this.Mensaje + " enviado por Whatsapp a " + Whatsapp;
+            return xnoti;
+        }
+    }
+
+
+}
